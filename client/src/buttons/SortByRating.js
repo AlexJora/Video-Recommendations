ad721bffd1a6fb6f73fbf9e12d66d87a27e4f807
AlexJora <jora.catalinaa@gmail.com>
2023-07-16 12:46:07 +0200

    sort

---
+++ client/src/buttons/SortByRating.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { FaSort } from "react-icons/fa";
+import { useGlobalContext } from "../context/VideoContext";
+function SortByRating({ handleSort }) {
+  return (
+    <div
+      className="sort"
+      style={{ width: "10rem", textAlign: "center", margin: "auto" }}
+    >
+      <div>
+        Rating
+        <FaSort
+          style={{ color: "blue", fontSize: "40px" }}
+          onClick={handleSort}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SortByRating;
